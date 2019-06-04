fb0d94f96af426ba502df94f869d5843a73d0215
erichuizapucp <eric.huiza@pucp.edu.pe>
2019-06-04 16:57:46 -0500

    initial updates to the Audio Handler class

--- video-transcription/audio/audio-handler.py
+++ video-transcription/audio/audio-handler.py
@@ -1,9 +1,14 @@
 import boto3
 import botocore
+import os
 
 
 class AudioHandler:
-    BUCKET_NAME = ""
+    def __init__(self):
+        self.bucketName = os.environ("S3_Bucket")
+        self.S3 = boto3.resource("s3")
+        self.Bucket = self.S3.Bucket(self.bucketName)
 
-    def handle(self):
+    def handle(self, video_id):
+        self.Bucket.download_file(video_id, "")
         print("handle audio extraction from Bucket")
