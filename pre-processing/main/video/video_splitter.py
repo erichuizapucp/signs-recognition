6050128b8487a52beaf5509423bfb92d05ed92aa
erichuizapucp <eric.huiza@pucp.edu.pe>
2019-07-10 19:59:36 -0500

    adding support for ffmpeg to create video chunks

--- pre-processing/main/video/video_splitter.py
+++ pre-processing/main/video/video_splitter.py
@@ -1,3 +1,10 @@
+from subprocess import check_call
+import shlex
+import os
+import ntpath
+from datetime import datetime
+import time
+
 from processor import Processor
 
 
@@ -6,6 +13,32 @@ class VideoSplitter(Processor):
         super().__init__()
 
     def process(self, data):
+        exec_start = time.time()
+
         super().process(data)
         seqs = data['sequences']
-        print(seqs)
+
+        [self.__split_video(seq['start'], seq['end'], idx) for idx, seq in enumerate(seqs)]
+
+        exec_end = time.time()
+        print('Video splittrer completed completed processing {} video chunks in {} seconds'.
+              format(len(seqs), round(exec_end - exec_start, 2)))
+
+    def __split_video(self, start, end, idx):
+        start_time = datetime.strptime(start, '%M:%S')
+        end_time = datetime.strptime(end, '%M:%S')
+
+        duration = \
+            datetime.combine(datetime.today(), end_time.time()) - datetime.combine(datetime.today(), start_time.time())
+
+        base_dir = os.path.dirname(self._video_file_path)
+        chunk_path = os.path.join(base_dir, 'chunks', str(idx) + '-' + ntpath.basename(self._video_file_path))
+
+        if not os.path.exists(os.path.dirname(chunk_path)):
+            os.makedirs(os.path.dirname(chunk_path), exist_ok=True)
+
+        cmd = "ffmpeg -i {} -vcodec copy  -strict -2 -ss {} -t {} {} -loglevel panic".format(
+            self._video_file_path, start_time.strftime('%H:%M:%S'), str(duration), chunk_path)
+
+        if check_call(shlex.split(cmd), universal_newlines=True) == 0:
+            print('{} was created successfully'.format(chunk_path))
