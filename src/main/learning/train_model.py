e843304af01770c92646d6665208eabbbd054411
erichuizapucp <eric.huiza@pucp.edu.pe>
2020-06-24 18:12:01 -0500

    added support for a model where the Opticalflow and RGB are freezed and pop that way we added a new layer on top of concat for those two models

--- src/main/learning/train_model.py
+++ src/main/learning/train_model.py
@@ -4,12 +4,13 @@ import os
 from learning.model.opticalflow_model_builder import OpticalFlowModelBuilder
 from learning.model.rgb_recurrent_model_builder import RGBRecurrentModelBuilder
 from learning.model.nsdm_builder import NSDMModelBuilder
+from learning.model.nsdm_v2_builder import NSDMV2ModelBuilder
 from argparse import ArgumentParser
 from logger_config import setup_logging
 from learning.execution.opticalflow_executor import OpticalflowExecutor
 from learning.execution.rgb_executor import RGBExecutor
 from learning.execution.nsdm_executor import NSDMExecutor
-from learning.common.model_type import OPTICAL_FLOW, RGB, NSDM
+from learning.common.model_type import OPTICAL_FLOW, RGB, NSDM, NSDMV2
 
 DEFAULT_NO_EPOCHS = 5
 DEFAULT_NO_STEPS_EPOCHS = None
@@ -55,11 +56,21 @@ def get_nsdm_model():
     return nsdm_model
 
 
+def get_nsdm_v2_model():
+    opticalflow_model = get_saved_model(OPTICAL_FLOW)
+    rgb_model = get_saved_model(RGB)
+
+    model_builder = NSDMV2ModelBuilder()
+    nsdm_v2_model = model_builder.build(OpticalflowModel=opticalflow_model, RGBModel=rgb_model)
+    return nsdm_v2_model
+
+
 def get_model(model_name):
     models = {
         OPTICAL_FLOW: lambda: get_opticalflow_model(),
         RGB: lambda: get_rgb_model(),
         NSDM: lambda: get_nsdm_model(),
+        NSDMV2: lambda: get_nsdm_v2_model(),
     }
     model = models[model_name]()
     return model
@@ -69,7 +80,8 @@ def get_executor(executor_name, model):
     executors = {
         OPTICAL_FLOW: lambda: OpticalflowExecutor(model),
         RGB: lambda: RGBExecutor(model),
-        NSDM: lambda: NSDMExecutor(model)
+        NSDM: lambda: NSDMExecutor(model),
+        NSDMV2: lambda: NSDMExecutor(model),
     }
     executor = executors[executor_name]()
     executor.configure()
