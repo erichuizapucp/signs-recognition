622138aa81e7edf2fb6eed6ce3fb905126ca861b
erichuizapucp <eric.huiza@pucp.edu.pe>
2020-06-25 15:25:44 -0500

    better support for nsdm v2 execution

--- src/main/learning/model/nsdm_builder.py
+++ src/main/learning/model/nsdm_builder.py
@@ -10,19 +10,20 @@ from learning.common.model_type import NSDM
 class NSDMModelBuilder(BaseModelBuilder):
     def __init__(self):
         super().__init__()
+
+        # input shape 224x224x3
+        self.opticalflow_input_shape = (self.imagenet_img_width, self.imagenet_img_height, self.imagenet_rgb_no_channels)
+        # feature dim calculated from 224 * 224 * 3
+        self.rgb_input_shape = (None, self.frames_seq_img_width * self.frames_seq_img_height * self.frames_seq_no_channels)
+
         self.logger = logging.getLogger(__name__)
 
     def build(self, **kwargs) -> Model:
         opticalflow_model = kwargs['OpticalflowModel']
         rgb_model = kwargs['RGBModel']
 
-        # input shape 224x224x3
-        opticalflow_input_shape = (self.imagenet_img_width, self.imagenet_img_height, self.imagenet_rgb_no_channels)
-        # feature dim calculated from 224 * 224 * 3
-        rgb_input_shape = (None, self.frames_seq_img_width * self.frames_seq_img_height * self.frames_seq_no_channels)
-
-        opticalflow_input = Input(shape=opticalflow_input_shape, name='opticalflow_inputs')
-        rgb_input = Input(shape=rgb_input_shape, name='rgb_inputs')
+        opticalflow_input = Input(shape=self.opticalflow_input_shape, name='opticalflow_inputs')
+        rgb_input = Input(shape=self.rgb_input_shape, name='rgb_inputs')
 
         opticalflow_target = opticalflow_model(opticalflow_input)
         rgb_target = rgb_model(rgb_input)
--- src/main/learning/model/nsdm_v2_builder.py
+++ src/main/learning/model/nsdm_v2_builder.py
@@ -1,13 +1,13 @@
 import logging
 
 from tensorflow.keras.models import Model, Sequential
-from learning.model.base_model_builder import BaseModelBuilder
+from learning.model.nsdm_builder import NSDMModelBuilder
 from tensorflow.keras import Input
 from tensorflow.keras.layers import Dense, Concatenate
 from learning.common.model_type import NSDMV2
 
 
-class NSDMV2ModelBuilder(BaseModelBuilder):
+class NSDMV2ModelBuilder(NSDMModelBuilder):
     def __init__(self):
         super().__init__()
         self.logger = logging.getLogger(__name__)
@@ -19,13 +19,8 @@ class NSDMV2ModelBuilder(BaseModelBuilder):
         opticalflow_model.trainable = False
         rgb_model.trainable = False
 
-        # input shape 224x224x3
-        opticalflow_input_shape = (self.imagenet_img_width, self.imagenet_img_height, self.imagenet_rgb_no_channels)
-        # feature dim calculated from 224 * 224 * 3
-        rgb_input_shape = (None, self.frames_seq_img_width * self.frames_seq_img_height * self.frames_seq_no_channels)
-
-        opticalflow_input = Input(shape=opticalflow_input_shape, name='opticalflow_inputs')
-        rgb_input = Input(shape=rgb_input_shape, name='rgb_inputs')
+        opticalflow_input = Input(shape=self.opticalflow_input_shape, name='opticalflow_inputs')
+        rgb_input = Input(shape=self.rgb_input_shape, name='rgb_inputs')
 
         opticalflow_model = Sequential(opticalflow_model.layers[:-1])
         rgb_model = Sequential(rgb_model.layers[:-1])
